308609fd6fb80bbcbb38ded683f25c73d696b164
Galvin <galvinnguyen123@gmail.com>
2026-04-26 01:09:10 +0700

    lesson 01: rbac level 1 1 user 1 role

---
+++ src/models/rbac-lever-1.js
@@ -0,0 +1,12 @@
+export const MOCK_ROLES_LEVEL_1 = {
+  ADMIN: 'admin',
+  CLIENT: 'client',
+  MODERATOR: 'moderator'
+}
+
+export const MOCK_DATABASE_USER_LEVEL_1 = {
+  ID: 'trungquandev-sample-id-12345678',
+  EMAIL: 'trungquandev.official@gmail.com',
+  PASSWORD: 'trungquandev@123',
+  ROLE: MOCK_ROLES_LEVEL_1.ADMIN
+}
--- src/routes/v1/dashboardRoute.js
+++ src/routes/v1/dashboardRoute.js
@@ -1,11 +1,35 @@
 // Author: TrungQuanDev: https://youtube.com/@trungquandev
 import express from 'express'
+import { StatusCodes } from 'http-status-codes'
 import { dashboardController } from '~/controllers/dashboardController'
 import { authMiddleware } from '~/middlewares/authMiddleware'
+import { rbacLevel1Middleware } from '~/middlewares/rbacLevel1Middleware'
+import { MOCK_ROLES_LEVEL_1 } from '~/models/rbac-lever-1'
 
 const Router = express.Router()
 
 Router.route('/access')
   .get(authMiddleware.isAuthorized, dashboardController.access)
+
+Router.route('/messages')
+  .get(
+    authMiddleware.isAuthorized,
+    rbacLevel1Middleware.isValidateRbacLevel1([
+      MOCK_ROLES_LEVEL_1.ADMIN,
+      MOCK_ROLES_LEVEL_1.MODERATOR
+    ]),
+    (req, res) => {
+      res.status(StatusCodes.OK).json({ message: 'Messages access successfully!' })
+    }
+  )
+
+Router.route('/admin-tools')
+  .get(
+    authMiddleware.isAuthorized,
+    rbacLevel1Middleware.isValidateRbacLevel1([MOCK_ROLES_LEVEL_1.ADMIN]),
+    (req, res) => {
+      res.status(StatusCodes.OK).json({ message: 'Admin tools access successfully!' })
+    }
+  )
 
 export const dashboardRoute = Router
